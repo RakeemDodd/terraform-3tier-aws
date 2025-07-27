ef029e0f44e933cc09627330cd54b2a7b11926b2
RakeemDodd <rakeemdodd@gmail.com>
2025-07-26 22:42:25 -0400

    Refactored project: added variables.tf and outputs.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 resource "aws_eip" "nat" {
@@ -9,19 +9,19 @@ resource "aws_eip" "nat" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.cidr_block
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
   enable_nat_gateway = true
   single_nat_gateway = false
-  reuse_nat_ips = true
-  external_nat_ip_ids = "${aws_eip.nat[*].id}"
+  reuse_nat_ips      = true
+  external_nat_ip_ids = aws_eip.nat[*].id
 
-  enable_vpn_gateway = false  # Set to true only if you actually need a VPN
+  enable_vpn_gateway = false
 
   tags = {
     Terraform   = "true"
