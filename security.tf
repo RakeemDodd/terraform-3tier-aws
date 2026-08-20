ce484c839da71dc2df9f67a305fae2d163da8be8
Rakeem Dodd <rakeemdodd@gmail.com>
2026-08-20 01:44:12 -0400

    Update SSH ingress rule to use placeholder IP
    Replaced hardcoded IP address with placeholder for user IP.

--- security.tf
+++ security.tf
@@ -25,7 +25,7 @@ resource "aws_security_group_rule" "allow_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["67.83.164.99/32"]
+  cidr_blocks       = ["YOUR_IP_HERE/32"]
   security_group_id = aws_security_group.ec2_sg.id
   description       = "Allow SSH from my IP"
 }
